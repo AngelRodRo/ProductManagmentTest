d5b1e50eb16232caae5c3195ed26b91b0ebc49a5
Angel <aleangrodriguez123@gmail.com>
2017-04-12 20:56:04 -0500

    Adding product form and create method

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Product;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('products');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+
+
+
+
+    }
+
+    public function saveProduct($product){
+        $products = json_decode(file_get_contents(storage_path()."products.json"));
+        $product["id"] = sizeof($products);
+        $products[] = $product;
+        file_put_contents(storage_path(), json_encode($products));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $req)
+    {
+        $data = $req->all();
+
+        $this->validate($req,[
+            'name' => 'required',
+            'quantity'=>'required',
+            'price' => 'required'
+        ]);
+
+        $product = array();
+        $product["name"] = $data["name"];
+        $product["quantity"]= $data["quantity"];
+        $product["price"] = $data["price"];
+        saveProduct($product);
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
